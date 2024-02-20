7715c13b1fc2455cdafee22266ee9a7c88441ab9
EC2 Default User <ec2-user@ip-172-31-13-230.us-east-2.compute.internal>
2024-02-20 05:12:24 +0000

    tables created

---
+++ server/db.js
@@ -0,0 +1,33 @@
+const pg = require('pg');
+
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acmedb');
+
+const createTables = async()=> {
+    const SQL = `
+        DROP TABLE IF EXISTS reservations;
+        DROP TABLE IF EXISTS customers;
+        DROP TABLE IF EXISTS restaurants;
+        CREATE TABLE customers(
+            id UUID PRIMARY KEY,
+            name VARCHAR(50) NOT NULL UNIQUE
+        );
+        CREATE TABLE restaurants(
+            id UUID PRIMARY KEY,
+            name VARCHAR(50) NOT NULL UNIQUE
+        );
+        CREATE TABLE reservations(
+            id UUID PRIMARY KEY,
+            date DATE NOT NULL,
+            party_count INTEGER DEFAULT 5 NOT NULL,
+            restaurants_id UUID REFERENCES restaurants(id) NOT NULL,
+            customers_id UUID REFERENCES customers(id) NOT NULL
+        );
+    `;
+    await client.query(SQL);
+};
+
+
+module.exports = {
+    client,
+    createTables
+};
